464ef89fc3150f89d2eedfaf5de6eb488e66cd23
Snychng <snychng@gmail.com>
2025-09-11 22:00:33 +0800

    fix: 移除调试模式下自动打开开发者工具

--- src-tauri/src/core/setup/mod.rs
+++ src-tauri/src/core/setup/mod.rs
@@ -17,8 +17,8 @@ pub fn default(
     main_window: WebviewWindow,
     preference_window: WebviewWindow,
 ) {
-    #[cfg(debug_assertions)]
-    main_window.open_devtools();
+    // #[cfg(debug_assertions)]
+    // main_window.open_devtools();
 
     platform(app_handle, main_window.clone(), preference_window.clone());
 }
